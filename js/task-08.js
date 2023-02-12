9718ddcc3ec06bf24dc314643fd250ef62bd4888
Alexander <yusovsky2@gmail.com>
2023-02-12 21:19:45 +0200

    hw 06 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formEl = document.querySelector(".login-form");
 
+function onSubmitForm(event) {
+  event.preventDefault();
+
+  if (
+    event.currentTarget.elements.email.value !== "" &&
+    event.currentTarget.elements.password.value !== ""
+  ) {
+    const formData = new FormData(event.currentTarget);
+    formData.forEach((x, y) => console.log(x, "---", y));
+    formEl[0].value = "";
+    formEl[1].value = "";
+  } else {
+    alert("Все поля должны быть заполнены!");
+  }
+}
+
+formEl.addEventListener("submit", onSubmitForm);
