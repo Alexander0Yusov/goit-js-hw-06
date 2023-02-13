74de8fe769b8363d726840a5ac082afd8c10a487
Alexander <yusovsky2@gmail.com>
2023-02-13 19:21:23 +0200

    fixed remarks

--- js/task-06.js
+++ js/task-06.js
@@ -2,9 +2,17 @@ const inputEl = document.querySelector("#validation-input");
 const passLength = Number(inputEl.dataset.length);
 
 function onBlurInputEl(event) {
-  event.target.value.length === passLength
-    ? inputEl.classList.add("valid")
-    : inputEl.classList.add("invalid");
+  if (event.target.value.length === passLength) {
+    if (inputEl.classList.contains("invalid")) {
+      inputEl.classList.remove("invalid");
+    }
+    inputEl.classList.add("valid");
+  } else {
+    if (inputEl.classList.contains("valid")) {
+      inputEl.classList.remove("valid");
+    }
+    inputEl.classList.add("invalid");
+  }
 }
 
 inputEl.addEventListener("blur", onBlurInputEl);
--- js/task-08.js
+++ js/task-08.js
@@ -3,12 +3,13 @@ const formEl = document.querySelector(".login-form");
 function onSubmitForm(event) {
   event.preventDefault();
 
-  if (
-    event.currentTarget.elements.email.value !== "" &&
-    event.currentTarget.elements.password.value !== ""
-  ) {
-    const formData = new FormData(event.currentTarget);
-    formData.forEach((x, y) => console.log(x, "---", y));
+  const email = event.currentTarget.elements.email.value;
+  const password = event.currentTarget.elements.password.value;
+
+  if (email !== "" && password !== "") {
+    // const formData = new FormData(event.currentTarget);
+    // formData.forEach((x, y) => console.log(x, "---", y));
+    console.log({ email, password });
     formEl[0].value = "";
     formEl[1].value = "";
   } else {
